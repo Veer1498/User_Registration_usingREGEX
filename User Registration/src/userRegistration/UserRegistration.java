548ae42be7853e248cbed5895120c2e7b247d530
Veer1498 <singavarapuveerendra@gmail.com>
2022-09-07 00:11:07 +0530

    [Veer] Added : Welcome Message

---
+++ User Registration/src/userRegistration/UserRegistration.java
@@ -0,0 +1,11 @@
+package userRegistration;
+
+public class UserRegistration {
+    public static void welcome(){
+        System.out.println("Welcome to User Registration Program");
+    }
+
+    public static void main(String[] args) {
+        welcome();
+    }
+}
