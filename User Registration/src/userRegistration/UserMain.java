0f1647ced31f737d4051c67fa507c4229b50f2ea
Veer1498 <singavarapuveerendra@gmail.com>
2022-09-07 00:27:24 +0530

    [Veer] Added : Ability to Enter First Name and Check Pattern Using REGEX

---
+++ User Registration/src/userRegistration/UserMain.java
@@ -0,0 +1,10 @@
+package userRegistration;
+
+public class UserMain {
+    public static void main(String[] args) {
+        UserRegistration userData = new UserRegistration();
+
+        userData.welcome();
+        userData.firstName();
+    }
+}
--- User Registration/src/userRegistration/UserRegistration.java
+++ User Registration/src/userRegistration/UserRegistration.java
@@ -1,11 +1,23 @@
 package userRegistration;
 
-public class UserRegistration {
-    public static void welcome(){
-        System.out.println("Welcome to User Registration Program");
-    }
+import java.util.Scanner;
+import java.util.regex.Pattern;
 
-    public static void main(String[] args) {
-        welcome();
+/**
+ * Given : User Registration System needs to ensure all validations are in place during the User Entry.
+ * Author : Veer
+ */
+
+public class UserRegistration {
+    Scanner sc = new Scanner(System.in);
+    public void welcome(){
+        System.out.println("Welcome to User Registration Program");
+        System.out.println("====================================");
+    }
+    public void firstName(){
+        System.out.println("Please Enter Valid First Name");
+        String firstName = sc.next();
+        System.out.println(Pattern.matches("[A-Z][a-z]{3,}",firstName));
+
     }
 }
