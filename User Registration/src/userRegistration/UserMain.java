d92cb0991adfd8309614741acae304110c87a187
Veer1498 <singavarapuveerendra@gmail.com>
2022-09-07 01:20:12 +0530

    [Veer] Added : Ability to Enter PassWord with Given Rule 3 and Check Pattern Using REGEX

--- User Registration/src/userRegistration/UserMain.java
+++ User Registration/src/userRegistration/UserMain.java
@@ -3,15 +3,21 @@ package userRegistration;
  * Given : User Registration System needs to ensure all validations are in place during the User Entry.
  * Author : Veer
  */
+/*
+STEPS :
+1. Created User Main class with main Function
+2. Created the Object of User Registration as userdata
+3. Calling All the Methods in That Class
+ */
 public class UserMain {
     public static void main(String[] args) {
         UserRegistration userData = new UserRegistration();
 
-//        userData.welcome();
-//        userData.firstName();
-//        userData.lastName();
-//        userData.email();
-//        userData.mobileNumber();
+        userData.welcome();
+        userData.firstName();
+        userData.lastName();
+        userData.email();
+        userData.mobileNumber();
         userData.password();
     }
 }
--- User Registration/src/userRegistration/UserRegistration.java
+++ User Registration/src/userRegistration/UserRegistration.java
@@ -8,37 +8,44 @@ import java.util.regex.Pattern;
  * Author : Veer
  */
 
+//User Registration Class
 public class UserRegistration {
     Scanner sc = new Scanner(System.in);
+
+    //Welcome Function
     public void welcome(){
         System.out.println("Welcome to User Registration Program");
         System.out.println("====================================");
     }
+    //firstName Function
     public void firstName(){
         System.out.println("Please Enter Valid First Name");
         String firstName = sc.next();
         System.out.println(Pattern.matches("[A-Z][a-z]{3,}",firstName));
 
     }
+    //LastName Function
     public void lastName() {
         System.out.println("Please Enter Valid Last Name");
         String lastName = sc.next();
         System.out.println(Pattern.matches("[A-Z][a-z]{3,}", lastName));
     }
+    //Email Function
     public void email() {
         System.out.println("Please Enter Valid Email");
         String email = sc.next(); //given : abc.xyz@bl.co.in
         System.out.println(Pattern.matches("[a-z]*.[a-z]*@[b]+[l]+.[c]+[o]+.[i]+[n]+", email));
     }
+    //Mobile Number Function
     public void mobileNumber() {
         System.out.println("Please Enter Valid Mobile Number");
         String number = sc.next(); //given : 91 9919819801
         System.out.println(Pattern.matches("[0-9]{2}\s[0-9]{10}", number));
     }
-
+    //password Function
     public void password() {
         System.out.println("Please Enter Valid Password");
         String password = sc.next();
-        System.out.println(Pattern.matches("[A-Z]+[A-z0-9]{7,}", password));
+        System.out.println(Pattern.matches("[A-Z]+[A-z0-9]{6,}[0-9]+", password));
     }
 }
